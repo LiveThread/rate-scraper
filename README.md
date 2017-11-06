d317c3ddb11ed252e91a8dd84a9f824197349a11
William Reed <wreed58@gmail.com>
2017-11-05 21:36:32 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+LiveThread's Rate Scraper
+
+A simple PRAW application to scrap reddit for data every so often and compare the comment rates to calculate comments / minute.
---
+++ models.py
@@ -0,0 +1,41 @@
+from sqlalchemy import Column, String, Float, Integer, Interval
+from sqlalchemy.ext.declarative import declarative_base
+import datetime
+from datatypes import UTCDateTime
+
+Base = declarative_base()
+
+
+# specific entries for the most recent calculated rate for the particular posting
+class Rate(Base):
+    __tablename__ = 'rates'
+
+    # the thing id like 't3_78n7ni'
+    submission_id = Column(String(length=9), primary_key=True)
+    # the rate in comments / minute
+    rate = Column(Float, nullable=False)
+    # can i auto set this somehow
+    timestamp = Column(UTCDateTime, nullable=False)
+    created = datetime.datetime
+
+# counts of comments at various times
+class Tracker(Base):
+    __tablename__ = 'tracker'
+
+    submission_id = Column(String(length=9), primary_key=True)
+    comment_count = Column(Integer, nullable=False)
+    # TODO: this default doesn't work. it gets set at start up and is constant. no good.
+    timestamp = Column(UTCDateTime, nullable=False, default=datetime.datetime.utcnow())
+
+
+# represents the peak comment rate that a certain thread hits
+# useful to analyse trends
+class PeakRate(Base):
+    __tablename__ = 'peak_rates'
+
+    # the thing id...
+    submission_id = Column(String(length=9), primary_key=True)
+    # the rate
+    rate = Column(Float, nullable=False)
+    # amount of time that occured until this post hit its peak rate
+    time_since_creation = Column(Interval, nullable=False)
